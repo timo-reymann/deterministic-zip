cea45adf54678720f9185dedf0c225ccff889250
Timo Reymann <Timo.Reymann@trustedshops.de>
2023-05-22 11:05:07 +0200

    fix(#15): Ignore own zip file if found in fileset

---
+++ pkg/features/fileset/ignore-target-zip_test.go
@@ -0,0 +1,82 @@
+package fileset
+
+import (
+	"github.com/timo-reymann/deterministic-zip/pkg/cli"
+	"reflect"
+	"testing"
+)
+
+func TestIgnoreTargetZipIsEnabled(t *testing.T) {
+	ignoreZip := IgnoreTargetZip{}
+	if !ignoreZip.IsEnabled(&cli.Configuration{}) {
+		t.Fatal("The feature should be enabled by default")
+	}
+}
+
+func TestIgnoreTargetZipDebugName(t *testing.T) {
+	testDebugName(t, (IgnoreTargetZip{}).DebugName(), "IgnoreTargetZip (builtin)")
+}
+
+func TestIgnoreTargetZipExecute(t *testing.T) {
+	ignoreZip := IgnoreTargetZip{}
+
+	testCases := []struct {
+		sources      []string
+		zipFile      string
+		err          *error
+		sourcesAfter []string
+	}{
+		{
+			zipFile: "test.zip",
+			sources: []string{
+				"testdata",
+				"test.zip",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+			},
+		},
+		{
+			zipFile: "test.zip",
+			sources: []string{
+				"testdata",
+				"test.zip",
+				"archive.zip",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+				"archive.zip",
+			},
+		},
+		{
+			zipFile: "test.zip",
+			sources: []string{
+				"testdata",
+				"test.zip",
+				"folder/test.zip",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+				"folder/test.zip",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		c := cli.Configuration{
+			ZipFile:     tc.zipFile,
+			SourceFiles: tc.sources,
+		}
+		err := ignoreZip.Execute(&c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(tc.sourcesAfter, c.SourceFiles) {
+			t.Fatalf("Expected %v, but got %v", tc.sourcesAfter, c.SourceFiles)
+		}
+	}
+}

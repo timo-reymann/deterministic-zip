e92b265f13aefb479a0c6e4801f848d29a0dd62b
Timo Reymann <timo.reymann@deepl.com>
2024-04-25 13:03:54 +0200

    feat(#35): Ignore non existent files

---
+++ pkg/features/fileset/ignore_non_existent_test.go
@@ -0,0 +1,74 @@
+package fileset
+
+import (
+	"github.com/timo-reymann/deterministic-zip/pkg/cli"
+	"reflect"
+	"testing"
+)
+
+func TestIgnoreNonExistent_IsEnabled(t *testing.T) {
+	ignoreNonExistent := IgnoreNonExistent{}
+	if !ignoreNonExistent.IsEnabled(&cli.Configuration{}) {
+		t.Fatal("The feature should be enabled by default")
+	}
+}
+
+func TestIgnoreNonExistent_DebugName(t *testing.T) {
+	testDebugName(t, (IgnoreNonExistent{}).DebugName(), "IgnoreNonExistent (builtin)")
+}
+
+func TestIgnoreNonExistent_Execute(t *testing.T) {
+	ignoreNonExistent := IgnoreNonExistent{}
+
+	testCases := []struct {
+		sources      []string
+		err          *error
+		sourcesAfter []string
+	}{
+		{
+			sources: []string{
+				"testdata",
+				"nonExistent",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+			},
+		},
+		{
+			sources: []string{
+				"testdata",
+				"testdata/file.txt",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+				"testdata/file.txt",
+			},
+		},
+		{
+			sources: []string{
+				"testdata",
+				"testdata/foo/file.txt",
+			},
+			err: nil,
+			sourcesAfter: []string{
+				"testdata",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		c := cli.Configuration{
+			SourceFiles: tc.sources,
+		}
+		err := ignoreNonExistent.Execute(&c)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if !reflect.DeepEqual(tc.sourcesAfter, c.SourceFiles) {
+			t.Fatalf("Expected %v, but got %v", tc.sourcesAfter, c.SourceFiles)
+		}
+	}
+}

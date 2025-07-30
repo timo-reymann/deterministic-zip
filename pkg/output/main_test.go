a43d8eb87348e34a329469f8b04defae4c23b312
Timo Reymann <mail@timo-reymann.de>
2025-07-30 21:17:58 +0200

    test: Add smoke tests for output methods

---
+++ pkg/output/main_test.go
@@ -0,0 +1,38 @@
+package output
+
+import "testing"
+
+func TestInfo(t *testing.T) {
+	Info("test")
+}
+
+func TestDebug(t *testing.T) {
+	Debug("test")
+}
+
+func TestLevelAndSetOutput(t *testing.T) {
+	// Test setting and getting log levels
+	originalLevel := Level()
+
+	// Test setting different levels
+	SetLevel(LevelInfo) // assuming you have SetLevel and level constants
+	if Level() != LevelInfo {
+		t.Errorf("Expected level %v, got %v", LevelInfo, Level())
+	}
+
+	SetLevel(LevelDebug)
+	if Level() != LevelDebug {
+		t.Errorf("Expected level %v, got %v", LevelInfo, Level())
+	}
+
+	// Restore original level
+	SetLevel(originalLevel)
+}
+
+func TestInfof(t *testing.T) {
+	Infof("test %s", "foo")
+}
+
+func TestDebugf(t *testing.T) {
+	Debugf("test %s", "foo")
+}

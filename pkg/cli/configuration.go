timo-reymann/deterministic-zip

commit f81022c2088c5a7014b0b9da5d9d134499121ff9

fix: args processing (#72)

pkg/cli/configuration.go
@@ -90,7 +90,7 @@ func (conf *Configuration) defineFlags() {
 }
 
 func (conf *Configuration) parseVarargs() error {
-	remaining := flag.Args()
+	remaining := conf.flagSet.Args()
 	if len(remaining) < 2 {
 		return ErrMinimalParamsMissing
 	}

commit 4c7c96a826c1339280a98d21131b673478ba6807

fix: [#74] Support shorthand flag `-h` for help again
This also broke with #72 and was overlooked.

pkg/cli/configuration.go
@@ -86,7 +86,6 @@ func (conf *Configuration) defineFlags() {
 	conf.addStringFlag(&conf.CompressionMethod, "compression-method", "Z", "deflate", "Set the default compression method. \nCurrently the main methods supported by zip are store and deflate. \nCompression method can be set to:\n\nstore       Setting the compression method to store forces to store entries with no compression. \n            This is generally faster than compressing entries, but results in no space savings.\n\ndeflate     This is the default method for zip. If zip determines that storing is better than deflation, the entry will be stored instead.\n")
 	conf.addStringFlag(&conf.LogFilePath, "logfile-path", "", "", "Open a logfile at the given path.\nBy default any existing file at that location is overwritten, but the --log-append option will result in an existing file being opened and the new log information appended to any existing information.")
 	conf.addBoolFlag(&conf.LogFileAppend, "log-append", "", false, "Append to existing logfile. Default is to overwrite.")
-	conf.flagSet.BoolP("Help", "h", false, "Show available commands")
 }
 
 func (conf *Configuration) parseVarargs() error {
@@ -126,7 +125,7 @@ func (conf *Configuration) CleanPaths() {
 func (conf *Configuration) Help() {
 	PrintCompactInfo()
 	println("deterministic-zip [-options] [zipfile list]")
-	flag.PrintDefaults()
+	conf.flagSet.PrintDefaults()
 }
 
 func (conf *Configuration) parseModifiedDate() (*time.Time, error) {
@@ -148,9 +147,10 @@ func (conf *Configuration) Parse() error {
 	conf.defineFlags()
 
 	isVersion := conf.flagSet.Bool("version", false, "Show version info")
+	isHelp := conf.flagSet.BoolP("Help", "h", false, "Show available commands")
 	err := conf.flagSet.Parse(os.Args[1:])
 
-	if errors.Is(err, flag.ErrHelp) {
+	if errors.Is(err, flag.ErrHelp) || (isHelp != nil && *isHelp) {
 		conf.Help()
 		return ErrAbort
 	} else if *isVersion {